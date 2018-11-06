73aa9cd74c9407d491f704cfccb9d8495a5e7393
Mercedes Gonzalez <mmgxw3@mst.edu>
2018-11-06 16:33:09 -0600

    Add comments, format plot

--- Main.m.m
+++ Main.m.m
@@ -14,14 +14,18 @@ addAnalogOutputChannel(s, 'myDAQ1',1,'Voltage');
 s.Rate = 10000; %scans per second
 %s.DurationInSeconds = 1;
 outputSignalValue = 1; % Volts per channel
-xlim([0 10]);
 
-%% Start at 100 degrees 
+%% Set plot parameters
+xlim([0 10]);
+xlabel('Time (s)');
+ylabel('Amplitude (V)');
+legend(['Measured','Output']);
+
+%% Move servo to 100 degrees
 set_param(gcs,'SimulationMode','external');
 set_param(gcs,'SimulationCommand','connect');
 set_param(gcs,'SimulationCommand','start');
 pause(15);
-
 set_param(gcs,'SimulationCommand','stop');
 
 %% Signal generation and reading
@@ -30,18 +34,27 @@ outputSignal1 = chirp(t,50,1,15000)';
 outputSignal2 = -chirp(t,50,1,15000)';
 
 for i = 1:37
-    inputAngle = 100-5*i
+    inputAngle = 100-5*i %degrees
     set_param('Simulate_it3/inputAngle','Value',num2str(inputAngle));
     set_param(gcs,'SimulationCommand','connect');
     set_param(gcs,'SimulationCommand','start');
     pause(3);
-    measuredTheta(i) = theta_l(end)
+    
+    %% Store theta information for later on
+    measuredTheta(i) = theta_l(end);
     inputTheta(i) = inputAngle;
+
     set_param(gcs,'SimulationCommand','stop');
     pause(1);
+    
+    %% Send output signal
     queueOutputData(s,[outputSignal1, outputSignal2]);
+    
+    %% Read signal
     [data,time] = s.startForeground;
-    plot(time,data)
+    
+    %% Plot measured data and output signals. 
+    plot(time,data,'r',time,outputSignal1,'b',time,outputSignal2,'b');
 end
 
 %outputSignal = sin(linspace(0,pi*2,s.Rate)');
