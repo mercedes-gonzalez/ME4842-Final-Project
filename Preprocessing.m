29f89718f2fac1cff379b2c1fed41ab0b17d61ba
mikerouleau <boymechanic1@gmail.com>
2018-11-10 03:00:50 -0600

    Started looking at raw data
    Started importing raw data into massive struct
    Started to validate experimental data by looking at fft

---
+++ Preprocessing.m
@@ -0,0 +1,28 @@
+clc
+clear variables
+pwd
+data(1) = load('..\Data\Project Data\reference_1_workspace.mat',...
+    'measuredTheta', 'outputSignal1', 'savedData', 'time', 'triggerTime');
+data(2) = load('..\Data\Project Data\reference_2_workspace.mat',...
+    'measuredTheta', 'outputSignal1', 'savedData', 'time', 'triggerTime');
+
+%%
+plot(data(1).savedData)
+hold on;
+plot(data(1).outputSignal1)
+hold off;
+
+% I think we need to swap columns 1 and 2 of all the data.savedData or specify column 2 somehow;
+% fft may be operating on first column - MR
+Y=fft(data(2).savedData) 
+L=40001
+Fs=400
+
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(L/2))/L;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
